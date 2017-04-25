dd1e951f815a9e1d02dead7f444725299ae89f76
Victoria Trzcianowski <vtrzcianowski@gmail.com>
2017-04-25 17:14:30 -0400

    end of day tuesday

--- java.js
+++ java.js
@@ -1,46 +1,47 @@
-/* Built by LucyBot. www.lucybot.com
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "441eb2940b574368a76954b91c4fa337"
-});*/
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({'api-key': "441eb2940b574368a76954b91c4fa337"});
+
 var selectedCategory;
+var url="https://api.nytimes.com/svc/topstories/v2/";
+var x=0;
 
-// var url = "https://api.nytimes.com/svc/topstories/v2/";
-// url += selectedCategory +".json"+'?' + $.param({'api-key': "441eb2940b574368a76954b91c4fa337"});
-// var selectedCategory;
+$(document).ready(function(){
+  $('#category').on('change', function(){
 
-function newArticle(abstract, image, link)
-{
-          // var articleClone = document.createElement("div");
-          // $(articleClone).html("<p>"+abstract+"</p> <a href="+link+">LINK</a> <img src=" +image+">");
-          // $("body").append(articleClone);
+    //clear previous results from body
+     selectedCategory = $(this).val();
+     var catUrl = url + selectedCategory +".json"+'?' + $.param({'api-key': "441eb2940b574368a76954b91c4fa337"});
+
+    $.ajax({
+      url: catUrl,
+      method: 'GET',
+    })
+    .done(function(result) {
+      $("#articlesSection").empty();
+      loop_articles(result.results);
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
+
+  });
+});
+function newArticle(abstract, image, link){
           var articleClone = $("#clone").clone();
-          //$(articleClone).html("<a href="+link+"><p>"+abstract+"</p></a>");
           $(articleClone).children(".article-wrap").css("background", "url('"+image+"') 50%");
           $(articleClone).find("p").text(abstract);
           $(articleClone).find("a").attr("href", link);
-          $("body").append($(articleClone).html());
+          $("#articlesSection").append($(articleClone).html());
 }
-
 function loop_articles(results){
   for (var i=0; i < results.length; i++){
-    newArticle(results[i].abstract, results[i].multimedia[4].url, results[i].url);
-  };
+
+    if(results[i].multimedia[4].url){
+      newArticle(results[i].abstract, results[i].multimedia[4].url, results[i].url);
+      x++;
+      console.log(x);
+
+          if(x==12){break;}
+
+    }
+
+  }
 }
-
-
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  // console.log(result)
-  //neo.jump(document.getElementById("jumpHeight").value);
-  //document.getElementById("category").addEventListener("click", loop_articles,false);
-
-  loop_articles(result.results);
-
-}).fail(function(err) {
-  throw err;
-});
