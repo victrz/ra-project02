43979acd65c6e441d75759fd4c2450fa58eeb0aa
Victoria Trzcianowski <vtrzcianowski@gmail.com>
2017-04-25 13:47:19 -0400

    tuesday starting to work on drop down

--- java.js
+++ java.js
@@ -3,14 +3,15 @@ var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 url += '?' + $.param({
   'api-key': "441eb2940b574368a76954b91c4fa337"
 });*/
-  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "441eb2940b574368a76954b91c4fa337"
-});
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({'api-key': "441eb2940b574368a76954b91c4fa337"});
+var selectedCategory;
 
- function newArticle(abstract, image, link)
+// var url = "https://api.nytimes.com/svc/topstories/v2/";
+// url += selectedCategory +".json"+'?' + $.param({'api-key': "441eb2940b574368a76954b91c4fa337"});
+// var selectedCategory;
 
-
+function newArticle(abstract, image, link)
 {
           // var articleClone = document.createElement("div");
           // $(articleClone).html("<p>"+abstract+"</p> <a href="+link+">LINK</a> <img src=" +image+">");
@@ -21,9 +22,7 @@ url += '?' + $.param({
           $(articleClone).find("p").text(abstract);
           $(articleClone).find("a").attr("href", link);
           $("body").append($(articleClone).html());
-
 }
-
 
 function loop_articles(results){
   for (var i=0; i < results.length; i++){
@@ -37,8 +36,8 @@ $.ajax({
   method: 'GET',
 }).done(function(result) {
   // console.log(result)
-  neo.jump(document.getElementById("jumpHeight").value);
-  document.getElementById("category").addEventListener("click", loop_articles,false);
+  //neo.jump(document.getElementById("jumpHeight").value);
+  //document.getElementById("category").addEventListener("click", loop_articles,false);
 
   loop_articles(result.results);
 
